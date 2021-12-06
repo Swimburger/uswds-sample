769e8d2cd815bd747e254a8cfad12f1998bb84a5
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2021-12-05 21:44:29 -0500

    Update code to use uswds PR as submodule

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import { modal } from '../node_modules/uswds/src/js/components';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import { modal } from '../uswds/src/js/components';
 
 export default function App() {
   const path = window.location.pathname;
@@ -20,24 +20,7 @@ export default function App() {
       }
       {path === '/modal2' &&
         <>
-          <button
-            type="button"
-            className="usa-button"
-            aria-controls="modal2-1"
-            data-open-modal
-          >
-            Open Modal 1
-          </button>
-          <button
-            type="button"
-            className="usa-button"
-            aria-controls="modal2-2"
-            data-open-modal
-          >
-            Open Modal 2
-          </button>
-          <Modal2 id="modal2-1" />
-          <Modal2 id="modal2-2" />
+          Modal 2 has been removed, it doesn't make sense with the fixes in place.
         </>
       }
       {path === '/modal3' &&
@@ -117,67 +100,10 @@ const ModalButton = function ({ modalId, children }) {
 
 const Modal1 = function ({ id }) {
   const modalRef = useRef();
-  useEffect(() => {
+  useLayoutEffect(() => {
     const modalElement = modalRef.current;
     modal.on(modalElement);
-    return () => {
-      modal.off(modalElement);
-    };
-  });
-  // empty div because React will try to remove .usa-modal from this components parent, but USWDS already moved .usa-modal elsewhere
-  return (
-    <div style={{ display: 'none' }}>
-      <div
-        className="usa-modal"
-        id={id}
-        aria-labelledby={`${id}-heading`}
-        aria-describedby={`${id}-description`}
-        ref={modalRef}
-      >
-        <div className="usa-modal__content">
-          <div className="usa-modal__main">
-            <h2 className="usa-modal__heading" id={`${id}-heading`}>
-              Lorem
-            </h2>
-            <div className="usa-prose">
-              <p id={`${id}-description`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus molestiae incidunt, sit eos reprehenderit, debitis culpa, omnis laboriosam repellat quas dolorem! Consequuntur nihil modi dolorum temporibus itaque? Pariatur, sunt itaque?</p>
-            </div>
-            <div className="usa-modal__footer">
-              <ul className="usa-button-group">
-                <li className="usa-button-group__item">
-                  <button type="button" className="usa-button" data-close-modal>
-                    Continue without saving
-                  </button>
-                </li>
-                <li className="usa-button-group__item">
-                  <button
-                    type="button"
-                    className="usa-button usa-button--unstyled padding-105 text-center"
-                    data-close-modal
-                  >
-                    Go back
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-const Modal2 = function ({ id }) {
-  const modalRef = useRef();
-  useEffect(() => {
-    const modalElement = modalRef.current;
-    // initialize all modals (same as passing in document.body)
-    modal.on();
-    return () => {
-      // de-initialize all modals (same as passing in document.body)
-      modal.off();
-      // remove modal wrapper element from document.body
-      document.body.removeChild(modalElement.parentElement.parentElement);
-    }
+    return () => modal.off(modalElement);
   });
   // empty div because React will try to remove .usa-modal from this components parent, but USWDS already moved .usa-modal elsewhere
   return (
@@ -224,16 +150,10 @@ const Modal2 = function ({ id }) {
 
 const ModalContainer = function ({ children }) {
   const modalContainerRef = useRef();
-  useEffect(() => {
+  useLayoutEffect(() => {
     const modalContainerElement = modalContainerRef.current;
-    const modalElements = modalContainerElement.querySelectorAll('.usa-modal');
     modal.on(modalContainerElement);
-    return () => {
-      modal.off(modalContainerElement);
-      modalElements.forEach((modalElement) => {
-        document.body.removeChild(modalElement.parentElement.parentElement);
-      });
-    };
+    return () => modal.off(modalContainerElement);
   });
   return (
     <div ref={modalContainerRef}>{children}</div>
